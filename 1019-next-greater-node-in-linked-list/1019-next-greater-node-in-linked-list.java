f33bb345f02269b8bdbfd776e55d5e1d93124f6e
rishi-240901 <vashistharishi2406@gmail.com>
2026-08-04 20:58:57 +0530

    Time: 26 ms (34.09%), Space: 49.4 MB (85.3%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -1,20 +1,49 @@
-
 class Solution {
+    public int sizeLL(ListNode head){
+        ListNode curr=head;
+        int count=0;
+        while(curr!=null){
+            count++;
+            curr=curr.next;
+        }
+        return count;
+    }
+    public ListNode reverseLL(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode next=null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
     public int[] nextLargerNodes(ListNode head) {
-     ArrayList<Integer> list=new ArrayList<>();
-         while(head!=null){
-            list.add(head.val);
-            head=head.next;
-        }
+        int size = sizeLL(head);
+        int[] arr = new int[size];
+        ListNode nhead=  reverseLL(head);
         Stack<Integer> st=new Stack<>();
-        int n=list.size();
-        int[] ans=new int[n];
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && list.get(i)>list.get(st.peek())){
-                ans[st.pop()]=list.get(i);
+        st.push(nhead.val);
+        ListNode curr=nhead.next;
+        int ptr=size-2;
+        while(ptr>=0){
+            int ele=curr.val;
+            curr=curr.next;
+
+            while(st.size()>0 && st.peek()<=ele){
+                st.pop();
             }
-            st.push(i);
+            if(st.size()==0){
+                arr[ptr]=0;
+            }
+            else{
+                arr[ptr] = st.peek();
+            }
+            st.push(ele);
+            ptr--;
         }
-        return ans;
+        return arr;
     }
 }
