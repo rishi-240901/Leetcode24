cb5523fa8288b51d408651c5b0bcbcd699a2dbfe
rishi-240901 <vashistharishi2406@gmail.com>
2026-08-18 23:43:12 +0530

    Time: 0 ms (100%), Space: 43.5 MB (86.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,27 +1,28 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //isko apprch dekhte h 1) iska transpose krdo toh row will become col then 2) reverse krdo
         int n=matrix.length;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                // 1-Transpose the matrix
-                int temp=matrix[i][j];
+                int k=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+                matrix[j][i]=k;
             }
         }
-        // 2) Reverse the matrix 
-        for(int i=0;i<n;i++){
-            int left=0;
-            int right=n-1;
-            while(left<right){
-            int temp=matrix[i][left];
-            matrix[i][left]=matrix[i][right];
-            matrix[i][right]=temp;
-            left++;
-           
-              right--;
-            }
+        for(int i=0;i<matrix.length;i++){
+             reverse(matrix[i]);
+        }
+        
+    }
+
+    public void reverse(int[] arr){
+        int st=0;
+        int end=arr.length-1;
+        while(st<=end){
+            int k=arr[st];
+            arr[st]=arr[end];
+            arr[end]=k;
+            st++;
+            end--;
         }
     }
 }
