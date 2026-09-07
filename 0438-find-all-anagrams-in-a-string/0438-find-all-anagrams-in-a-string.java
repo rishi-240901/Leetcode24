93ef522e3add236dac1769cb84e91678d229304e
rishi-240901 <vashistharishi2406@gmail.com>
2026-09-07 10:53:32 +0530

    Time: 9 ms (92.17%), Space: 46.6 MB (91.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        int n1 = s.length();  
+        int n2 = p.length();  
+
         List<Integer> ans = new ArrayList<>();
-        if(s.length()<p.length()){
-            return ans;
+
+        if (n1 < n2) return ans;
+
+        int[] a = new int[26]; 
+        int[] b = new int[26]; 
+
+        for (int i = 0; i < n2; i++) {
+            a[p.charAt(i) - 'a']++;
+            b[s.charAt(i) - 'a']++;
         }
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char ch : p.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        int k=p.length();
-        HashMap<Character,Integer> window = new HashMap<>();
-        for(int i = 0 ; i < k ; i++) {
-            char ch = s.charAt(i);
-            window.put(ch,window.getOrDefault(ch,0)+1);
-        }
-        if(map.equals(window)) {
+
+        if (Arrays.equals(a, b)) {
             ans.add(0);
         }
-        for(int i=k ; i < s.length() ; i++) {
-            char add = s.charAt(i);
-            window.put(add,window.getOrDefault(add,0)+1);
 
-            char remove = s.charAt(i-k);
-            window.put(remove,window.get(remove)-1);
+        for (int i = n2; i < n1; i++) {
+            b[s.charAt(i) - 'a']++;           
+            b[s.charAt(i - n2) - 'a']--;     
 
-            if(window.get(remove) == 0) {
-                window.remove(remove);
-            }
-            if (map.equals(window)) {
-                ans.add(i - k + 1);
+            if (Arrays.equals(a, b)) {
+                ans.add(i - n2 + 1);
             }
         }
+
         return ans;
     }
 }
